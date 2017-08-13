8e140580bb2ed4cd6927d0219cd719595ec99f54
mks-a <8500434+mks-a@users.noreply.github.com>
2017-08-13 17:23:12 +0300

    Changes 20170813
    Changes 20170813

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Code was tested in STM32F103C8
+This code will set led (on PC14) on and off on timer2 interrupt
+This code is not working yet
---
+++ code/main.s
@@ -0,0 +1,93 @@
+.thumb
+@This turns on Unified Assembly Language which is required to get all the features of Thumb-2.
+.syntax unified					@ without this line "orr r0, r0, #0x200000" will give an error "unshifted register required" 
+.cpu cortex-m3
+
+.equ STACKINIT, 0x20005000		@ end of SRAM 20kb
+
+.section .text
+.org 0x00000000					@ this not work!!!
+SP: .word STACKINIT				@ 20kb of RAM, so I will put stack pointer to the end of it
+RESET: .word main
+NMI_HANDLER: .word nmi_fault
+HARD_FAULT: .word hard_fault
+MEMORY_FAULT: .word memory_fault
+BUS_FAULT: .word bus_fault
+USAGE_FAULT: .word usage_fault
+.org 0x000000D0
+SPI2_INTERRUPT: .word spi2_interrupt + 1
+
+.ifndef SLEEP_MODES_DEF
+.include "stm32f103c8t6_core/sleep_modes.inc"
+.endif
+
+.ifndef CPU_DEF
+.include "stm32f103c8t6_core/cpu.inc"
+.endif
+
+.ifndef SDCARD_DEF
+.include "sdcard_reader/sdcard_reader.inc"
+.endif
+
+@.balign 2 				@ if bit 0 of address is 1 this indicate Thumb state of CPU, for Cortex-M it always should be 1, becaus it not support ARM state
+main:
+
+	push {lr}
+	bl cpu_init
+	pop {lr}
+	
+	push {lr}
+	bl sleep_mode_init
+	pop {lr}
+	
+	push {lr}
+	bl sdcard_reader_init
+	pop {lr}
+	
+	cpsie i						@ enable interrupts
+	
+_main_loop:
+
+	wfi 						@ wait for interrupt
+	
+	b _main_loop				@ branch to _main_loop and not load return address to link register (LR)
+	
+	@ return from function
+	bx lr						@ indirect branch to link register address
+
+nmi_fault:
+	@ breakpoint
+	bkpt
+	bx lr
+	
+hard_fault:
+	@ breakpoint
+	bkpt
+	bx lr
+
+memory_fault:
+	@ breakpoint
+	bkpt
+	bx lr
+
+bus_fault:
+	@ breakpoint
+	bkpt
+	bx lr
+
+usage_fault:
+	@ breakpoint
+	bkpt
+	bx lr
+	
+spi2_interrupt:
+	bx lr
+
+@ this is dummy function that just return from interrupt
+returtn_form_interrupt:
+	@ breakpoint
+	bkpt
+	bx lr
+	
+.end
+	
